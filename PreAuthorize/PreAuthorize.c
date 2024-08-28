1d95005c3ccd64f9383237401f245480d820bd48
Satish <satish@technotip.org>
2024-08-28 16:19:42 +0200

    Blob -> HookParameter
    Since it's a Payment transaction, the data can't be sent as a blob, so choose HookParameter instead.

--- PreAuthorize/PreAuthorize.c
+++ PreAuthorize/PreAuthorize.c
@@ -12,30 +12,31 @@ int64_t hook(uint32_t reserved) {
     if(equal)
         accept(SBUF("Pre-Authorize: Outgoing Transaction."), 1);    
 
-    uint8_t publickey[] = { 0xCAU, 0xFEU };
+    uint8_t publickey[1] = { 0x50U };
     uint8_t publickey_ptr[33];
-    if(hook_param(publickey_ptr, 33, publickey, 2) != 33)
+    if(hook_param(publickey_ptr, 33, publickey, 1) != 33)
         rollback(SBUF("Pre-Authorize: PublicKey not set as Hook parameter"), 2);  
 
-    uint8_t blob[100];
-    if(otxn_field(blob - 1, 101, sfBlob) != 101)
-        rollback(SBUF("Pre-Authorize: Blob must be 64 + 20 + 8 + 8 = 100 bytes."), 3);    
+    uint8_t inputKey[2] = { 0xCAU, 0xFEU };
+    uint8_t input[100];
+    if(otxn_param(input, 100, inputKey, 2) != 100)
+        rollback(SBUF("Pre-Authorize: Blob must be 64 + 20 + 8 + 8 = 100 bytes."), 3);   
 
-    if(util_verify(blob + 64, 36, blob, 64, publickey_ptr, 33) != 1) {
+    if(util_verify(input + 64, 36, input, 64, publickey_ptr, 33) != 1) {
         rollback(SBUF("Pre-Authorize: Unauthorized Transaction."), 4);   
     }      
 
-    BUFFER_EQUAL(equal, blob + 64, user_acc_id, 20);
+    BUFFER_EQUAL(equal, input + 64, user_acc_id, 20);
     if(!equal)
         rollback(SBUF("Pre-Authorize: Unauthorized Account."), 5);  
 
-    int64_t amount   = *((int64_t*)blob[84]);
+    int64_t amount   = *((int64_t*)input[84]);
     int64_t txn_amount;
     otxn_field(SVAR(txn_amount), sfAmount);    
     if(float_compare(amount, txn_amount, COMPARE_EQUAL) == 0)
         rollback(SBUF("Pre-Authorize: Unauthorized Amount."), 6);
 
-    int64_t sequence = *((int64_t*)blob[92]);
+    int64_t sequence = *((int64_t*)input[92]);
     int64_t user_acc_seq;
     otxn_field(SVAR(user_acc_seq), sfSequence);
     if(float_compare(sequence, user_acc_seq, COMPARE_EQUAL) == 0)
