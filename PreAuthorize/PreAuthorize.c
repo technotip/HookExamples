f77abd7a63b8d2b25d598b4b5345c5866625fe06
Satish <satish@technotip.org>
2024-08-29 13:31:49 +0200

    Add files via upload

--- PreAuthorize/PreAuthorize.c
+++ PreAuthorize/PreAuthorize.c
@@ -12,37 +12,46 @@ int64_t hook(uint32_t reserved) {
     if(equal)
         accept(SBUF("Pre-Authorize: Outgoing Transaction."), 1);    
 
+    uint32_t dest_tag;
+    if(otxn_field(SVAR(dest_tag), sfDestinationTag) != 4) 
+        rollback(SBUF("Pre-Authorize: Destination Tag Missing"), 2);
+
     uint8_t publickey[1] = { 0x50U };
     uint8_t publickey_ptr[33];
     if(hook_param(publickey_ptr, 33, publickey, 1) != 33)
-        rollback(SBUF("Pre-Authorize: PublicKey not set as Hook parameter"), 2);  
+        rollback(SBUF("Pre-Authorize: PublicKey not set as Hook parameter"), 3);  
+
 
     uint8_t inputKey[1] = { 0x49U };
-    uint8_t input[100];
-    if(otxn_param(input, 100, inputKey, 1) != 100)
-        rollback(SBUF("Pre-Authorize: Blob must be 64 + 20 + 8 + 8 = 100 bytes."), 3);   
+    uint8_t input[104];
+    if(otxn_param(input, 104, inputKey, 1) != 104)
+        rollback(SBUF("Pre-Authorize: HookParameter must be 64 + 20 + 4 + 8 + 8 = 104 bytes."), 4);   
 
-    if(util_verify(input + 64, 36, input, 64, publickey_ptr, 33) != 1) {
-        rollback(SBUF("Pre-Authorize: Unauthorized Transaction."), 4);   
-    }      
+    if(util_verify(input + 64, 40, input, 64, publickey_ptr, 33) != 1)
+        rollback(SBUF("Pre-Authorize: Unauthorized Transaction."), 5);   
 
     BUFFER_EQUAL(equal, input + 64, user_acc_id, 20);
     if(!equal)
-        rollback(SBUF("Pre-Authorize: Unauthorized Account."), 5);  
+        rollback(SBUF("Pre-Authorize: Unauthorized Account."), 6);  
 
-    int64_t amount   = *((int64_t*)input[84]);
-    int64_t txn_amount;
-    otxn_field(SVAR(txn_amount), sfAmount);    
+    uint32_t dest = *((int32_t*)input[84]);
+    if(float_compare(dest_tag, dest, COMPARE_EQUAL) == 0)
+        rollback(SBUF("Pre-Authorize: Wrong Recipient."), 7);        
+
+    uint64_t amount   = *((int64_t*)input[88]);
+    uint8_t amount_buf[48];
+    otxn_field(SBUF(amount_buf), sfAmount);    
+    uint64_t txn_amount = *((int64_t*)amount_buf);
     if(float_compare(amount, txn_amount, COMPARE_EQUAL) == 0)
-        rollback(SBUF("Pre-Authorize: Unauthorized Amount."), 6);
+        rollback(SBUF("Pre-Authorize: Unauthorized Amount."), 8);
 
-    int64_t sequence = *((int64_t*)input[92]);
-    int64_t user_acc_seq;
+    uint64_t sequence = *((int64_t*)input[96]);
+    uint64_t user_acc_seq;
     otxn_field(SVAR(user_acc_seq), sfSequence);
     if(float_compare(sequence, user_acc_seq, COMPARE_EQUAL) == 0)
-        rollback(SBUF("Pre-Authorize: Authorization Previously Used."), 7);
+        rollback(SBUF("Pre-Authorize: Authorization Previously Used."), 9);
   
-    accept(SBUF("Pre-Authorize: Payment Verified and Accepted."), 8);
+    accept(SBUF("Pre-Authorize: Payment Verified and Accepted."), 10);
     _g(1,1);
     return 0;    
 }
