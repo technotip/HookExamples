cf35c7a81908a73ffd0eb86034966374e106f584
Satish <satish@technotip.org>
2024-11-01 15:40:37 +0100

    Add files via upload

--- ReturnChange/radio.c
+++ ReturnChange/radio.c
@@ -50,51 +50,47 @@ int64_t hook(uint32_t) {
        NOPE("Xahau Radio: Less than allowed( 1 XAH ) minimum.");
 
 
-   if(float_compare(amount_xfl, 6089866696204910592, COMPARE_GREATER) == 1) {
+   uint64_t fraction_part  = (otxn_drops % (uint64_t) DROPS);
+   if(fraction_part == 0) 
+       DONE("Xahau Radio: Successful Request.");
 
-    uint64_t fraction_part  = (otxn_drops % (uint64_t) DROPS);
-    if(fraction_part == 0) 
-        DONE("Xahau Radio: Successful Request.");
+   {
+       uint64_t drops = fraction_part; //float_int(tempState.amount_xfl, 6, 1); 
+       uint8_t *b = AMOUNT_OUT; 
+       *b++ = 0b01000000 + ((drops >> 56) & 0b00111111); 
+       *b++ = (drops >> 48) & 0xFFU; 
+       *b++ = (drops >> 40) & 0xFFU; 
+       *b++ = (drops >> 32) & 0xFFU; 
+       *b++ = (drops >> 24) & 0xFFU; 
+       *b++ = (drops >> 16) & 0xFFU; 
+       *b++ = (drops >> 8) & 0xFFU; 
+       *b++ = (drops >> 0) & 0xFFU; 
+   }        
 
+    uint32_t current_ledger =  ledger_seq();
+    uint32_t fls = current_ledger + 1;
+    uint32_t lls = fls + 4;
+    etxn_reserve(1);
+
+    *((uint32_t *)(FLS_OUT)) = FLIP_ENDIAN(fls);
+    *((uint32_t *)(LLS_OUT)) = FLIP_ENDIAN(lls);
+    etxn_details(EMIT_OUT, 116U);
     {
-        uint64_t drops = fraction_part; //float_int(tempState.amount_xfl, 6, 1); 
-        uint8_t *b = AMOUNT_OUT; 
-        *b++ = 0b01000000 + ((drops >> 56) & 0b00111111); 
-        *b++ = (drops >> 48) & 0xFFU; 
-        *b++ = (drops >> 40) & 0xFFU; 
-        *b++ = (drops >> 32) & 0xFFU; 
-        *b++ = (drops >> 24) & 0xFFU; 
-        *b++ = (drops >> 16) & 0xFFU; 
-        *b++ = (drops >> 8) & 0xFFU; 
-        *b++ = (drops >> 0) & 0xFFU; 
-    }        
+        int64_t fee = etxn_fee_base(SBUF(txn));
+        uint8_t *b = FEE_OUT;
+        *b++ = 0b01000000 + ((fee >> 56) & 0b00111111);
+        *b++ = (fee >> 48) & 0xFFU;
+        *b++ = (fee >> 40) & 0xFFU;
+        *b++ = (fee >> 32) & 0xFFU;
+        *b++ = (fee >> 24) & 0xFFU;
+        *b++ = (fee >> 16) & 0xFFU;
+        *b++ = (fee >> 8) & 0xFFU;
+        *b++ = (fee >> 0) & 0xFFU;
+    }
 
-        uint32_t current_ledger =  ledger_seq();
-        uint32_t fls = current_ledger + 1;
-        uint32_t lls = fls + 4;
-        etxn_reserve(1);
-
-        *((uint32_t *)(FLS_OUT)) = FLIP_ENDIAN(fls);
-        *((uint32_t *)(LLS_OUT)) = FLIP_ENDIAN(lls);
-        etxn_details(EMIT_OUT, 116U);
-        {
-            int64_t fee = etxn_fee_base(SBUF(txn));
-            uint8_t *b = FEE_OUT;
-            *b++ = 0b01000000 + ((fee >> 56) & 0b00111111);
-            *b++ = (fee >> 48) & 0xFFU;
-            *b++ = (fee >> 40) & 0xFFU;
-            *b++ = (fee >> 32) & 0xFFU;
-            *b++ = (fee >> 24) & 0xFFU;
-            *b++ = (fee >> 16) & 0xFFU;
-            *b++ = (fee >> 8) & 0xFFU;
-            *b++ = (fee >> 0) & 0xFFU;
-        }
-
-        uint8_t emithash[32];
-        if(emit(SBUF(emithash), SBUF(txn)) != 32)
-            NOPE("Xahau Radio: Failed To Emit.");        
-
-   }
+    uint8_t emithash[32];
+    if(emit(SBUF(emithash), SBUF(txn)) != 32)
+        NOPE("Xahau Radio: Failed To Emit.");        
 
     DONE("Xahau Radio: Successful Request.");
 
